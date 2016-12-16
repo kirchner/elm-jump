4d7ddfcc803975b4c90273c086a886356fc8f5d3
Fabian Kirchner <kirchner@posteo.de>
2016-12-17 00:34:13 +0100

    add falling

--- src/Main.elm
+++ src/Main.elm
@@ -3,7 +3,18 @@ module Main exposing (..)
 import AnimationFrame
 import Html exposing (Html)
 import Html.Events as Events
-import Svg
+import Keyboard
+import Math.Vector2
+    exposing
+        ( Vec2
+        , add
+        , getX
+        , getY
+        , scale
+        , toTuple
+        , vec2
+        )
+import Svg exposing (Svg)
 import Svg.Attributes as Svg
 import Task
 import Time exposing (Time)
@@ -34,12 +45,31 @@ defaultModel =
 
 
 type alias State =
-    { counter : Int }
+    { position : Vec2
+    , velocity : Float
+    , ground : List Line
+    }
 
 
 defaultState : State
 defaultState =
-    { counter = 0 }
+    { position = vec2 500 50
+    , velocity = 0
+    , ground =
+        [ { left = vec2 0 300
+          , right = vec2 600 400
+          }
+        , { left = vec2 400 200
+          , right = vec2 600 100
+          }
+        ]
+    }
+
+
+type alias Line =
+    { left : Vec2
+    , right : Vec2
+    }
 
 
 
@@ -48,10 +78,46 @@ defaultState =
 
 step : Time -> State -> State
 step dt state =
-    { state
-        | counter =
-            state.counter + (round <| Time.inMilliseconds dt)
-    }
+    let
+        newPosition =
+            scale (state.velocity * Time.inMilliseconds dt) (vec2 1 0)
+                |> add state.position
+    in
+        stepFall dt { state | position = newPosition }
+
+
+stepFall : Time -> State -> State
+stepFall dt state =
+    let
+        newPosition =
+            vec2
+                (getX state.position)
+                (0.2 * (Time.inMilliseconds dt) + getY state.position)
+    in
+        collide state.position { state | position = newPosition }
+
+
+collide : Vec2 -> State -> State
+collide oldPosition state =
+    let
+        iy line =
+            (getX oldPosition - getX line.left)
+                * (getY line.right - getY line.left)
+                / (getX line.right - getX line.left)
+                + getY line.left
+
+        adjust line newPosition =
+            if
+                ((getY oldPosition - 2) <= iy line)
+                    && ((getY newPosition + 2) > iy line)
+                    && (getX oldPosition >= (getX line.left))
+                    && (getX oldPosition <= (getX line.right))
+            then
+                vec2 (getX newPosition) (iy line)
+            else
+                newPosition
+    in
+        { state | position = List.foldr adjust state.position state.ground }
 
 
 
@@ -64,22 +130,41 @@ draw dt state =
         newState =
             step dt state
 
-        offset =
-            sin <| (toFloat newState.counter / 1000)
+        ( x, y ) =
+            toTuple newState.position
     in
         Svg.svg
             [ Svg.width "600"
             , Svg.height "400"
             , Svg.viewBox "0 0 600 400"
             ]
-            [ Svg.circle
-                [ Svg.cx "300"
-                , Svg.cy "200"
-                , Svg.r <| toString <| 55 + 50 * offset
+            [ Svg.rect
+                [ Svg.x <| toString x
+                , Svg.y <| toString (y - 20)
+                , Svg.width "20"
+                , Svg.height "20"
                 , Svg.fill "green"
                 ]
                 []
+            , drawGround state
             ]
+
+
+drawGround : State -> Svg m
+drawGround state =
+    let
+        drawLine line =
+            Svg.line
+                [ Svg.x1 <| toString <| getX line.left
+                , Svg.y1 <| toString <| getY line.left
+                , Svg.x2 <| toString <| getX line.right
+                , Svg.y2 <| toString <| getY line.right
+                , Svg.width "1"
+                , Svg.stroke "brown"
+                ]
+                []
+    in
+        Svg.g [] <| List.map drawLine state.ground
 
 
 
@@ -96,6 +181,16 @@ type Msg
     | Run
     | Stop
     | Redraw Time
+    | KeyDown Keyboard.KeyCode
+    | KeyUp Keyboard.KeyCode
+    | Jump
+    | Move Direction
+    | Halt
+
+
+type Direction
+    = Left
+    | Right
 
 
 
@@ -181,6 +276,66 @@ update msg model =
         Redraw currentTime ->
             { model | currentTime = currentTime } ! []
 
+        KeyDown keycode ->
+            case keycode of
+                -- spacebar
+                32 ->
+                    update Jump model
+
+                -- left
+                37 ->
+                    update (Move Left) model
+
+                -- right
+                39 ->
+                    update (Move Right) model
+
+                _ ->
+                    model ! []
+
+        KeyUp keycode ->
+            case keycode of
+                -- left
+                37 ->
+                    update Halt model
+
+                -- right
+                39 ->
+                    update Halt model
+
+                _ ->
+                    model ! []
+
+        Jump ->
+            model ! []
+
+        Move direction ->
+            let
+                newVelocity =
+                    case direction of
+                        Left ->
+                            -0.1
+
+                        Right ->
+                            0.1
+
+                newState =
+                    model.state |> (\state -> { state | velocity = newVelocity })
+            in
+                { model | state = newState }
+                    ! [ Task.perform Tick Time.now ]
+
+        Halt ->
+            let
+                newVelocity =
+                    0
+
+                newState =
+                    model.state |> (\state -> { state | velocity = newVelocity })
+            in
+                { model | state = newState }
+                    ! [ Task.perform Tick Time.now ]
+
 
 
 -- SUBSCRIPTIONS
@@ -189,19 +344,29 @@ update msg model =
 subscriptions : Model -> Sub Msg
 subscriptions model =
     let
-        tick =
-            if model.paused then
-                Sub.none
-            else
-                Time.every (2000 * Time.millisecond) Tick
-
+        --        tick =
+        --            if model.paused then
+        --                Sub.none
+        --            else
+        --                Time.every (100 * Time.millisecond) Tick
         redraw =
             if model.paused then
                 Sub.none
             else
-                AnimationFrame.times Redraw
+                AnimationFrame.times Tick
+
+        keyDowns =
+            Keyboard.downs KeyDown
+
+        keyUps =
+            Keyboard.ups KeyUp
     in
-        Sub.batch [ tick, redraw ]
+        Sub.batch
+            --            [ tick
+            [ redraw
+            , keyDowns
+            , keyUps
+            ]
 
 
 
@@ -225,6 +390,9 @@ view model =
                   , Html.button
                         [ Events.onClick Run ]
                         [ Html.text "run" ]
+                  , Html.button
+                        [ Events.onClick Jump ]
+                        [ Html.text "jump" ]
                   , Html.div [] [ Html.text <| "current time: " ++ (toString model.currentTime) ]
                   , Html.div [] [ Html.text <| "state time: " ++ (toString model.stateTime) ]
                   ]
