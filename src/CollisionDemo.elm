bef504508eafb1e8cc69db3c5c2aaea683a99211
Fabian Kirchner <kirchner@posteo.de>
2017-01-08 10:50:41 +0100

    add rotation of player

---
+++ src/CollisionDemo.elm
@@ -0,0 +1,560 @@
+{- elm-jump - some platformer idea
+   Copyright © 2016 Fabian Kirchner
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
+
+module Main exposing (..)
+
+import Html exposing (Html)
+import Html.Events as Events
+import Keyboard
+import Math.Vector2 as Vec2
+    exposing
+        ( Vec2
+        , add
+        , getX
+        , getY
+        , scale
+        , toTuple
+        , vec2
+        )
+import Svg exposing (Svg)
+import Svg.Attributes as Svg
+import Time exposing (Time)
+import Engine
+import Camera exposing (defaultCamera)
+import Player exposing (..)
+import Http
+import Json.Decode as Json
+import Platform.Cmd as Cmd
+import Task
+
+
+-- STATE
+
+
+type alias State =
+    { movement : Maybe Direction
+    , ground : List Line
+    , tcamera : Camera.T
+    , players : List Player
+    , activePlayer : Int
+    }
+
+
+defaultState : State
+defaultState =
+    { movement = Nothing
+    , ground =
+        [ { left = vec2 0 100
+          , right = vec2 600 200
+          }
+        ]
+    , tcamera =
+        Camera.defaultT
+    , players =
+        [ { defaultPlayer
+            | position = vec2 300 0
+            , height = 20
+            , color = "#ffcc00"
+            , active = True
+          }
+        ]
+    , activePlayer = 0
+    }
+
+
+type alias Line =
+    { left : Vec2
+    , right : Vec2
+    }
+
+
+
+-- STEP
+
+
+{-| Propagate the State by the given Time.  This includes applying
+gravity force and the movements.
+-}
+physics : Time -> State -> State
+physics dt state =
+    state
+        |> movement dt
+        |> gravity dt
+
+
+gravity : Time -> State -> State
+gravity dt state =
+    let
+        acceleration =
+            0.0004
+
+        players =
+            state.players
+                |> List.map
+                    (\player ->
+                        let
+                            newVelocity =
+                                acceleration * (Time.inMilliseconds dt) + player.velocity
+
+                            newPosition =
+                                if player.falling then
+                                    vec2 0 1
+                                        |> scale (newVelocity * (Time.inMilliseconds dt))
+                                        |> add player.position
+                                else
+                                    player.position
+                        in
+                            { player
+                                | position = newPosition
+                                , velocity = newVelocity
+                            }
+                    )
+    in
+        { state
+            | players = players
+        }
+
+
+movement : Time -> State -> State
+movement dt state =
+    let
+        speed =
+            0.1
+
+        rotationSpeed =
+            0.001
+
+        direction =
+            case state.movement of
+                Just Left ->
+                    -1
+
+                Just Right ->
+                    1
+
+                Nothing ->
+                    0
+
+        directionVector =
+            vec2 direction 0
+
+        players =
+            state.players
+                |> List.indexedMap
+                    (\i player ->
+                        if i == state.activePlayer then
+                            let
+                                newPosition =
+                                    directionVector
+                                        |> scale (speed * (Time.inMilliseconds dt))
+                                        |> add player.position
+
+                                newRotation =
+                                    -- TODO: check if player is falling
+                                    if player.velocity > 0 then
+                                        player.rotation - direction * (Time.inMilliseconds dt) * rotationSpeed
+                                    else
+                                        player.rotation
+                            in
+                                { player
+                                    | position = newPosition
+                                    , rotation = newRotation
+                                }
+                        else
+                            player
+                    )
+    in
+        { state | players = players }
+
+
+{-| Check for collisions and adapt the state accordingly.
+-}
+collision : Time -> State -> State -> State
+collision dt oldState newState =
+    let
+        lines =
+            List.concat
+                [ newState.ground
+                , oldState.players
+                    |> List.map
+                        (\player ->
+                            { left = player.position |> add (vec2 (-10) (-player.height))
+                            , right = player.position |> add (vec2 10 (-player.height))
+                            }
+                        )
+                ]
+
+        players =
+            newState.players
+                |> List.map2 (,) oldState.players
+                |> List.map
+                    (\( oldPlayer, newPlayer ) ->
+                        let
+                            translation newPosition =
+                                { a = oldPlayer.position
+                                , b = newPosition
+                                }
+
+                            lineSegment line =
+                                { a = line.left
+                                , b = line.right
+                                }
+
+                            adjust line ( newPosition, newFalling ) =
+                                case intersection (translation newPosition) (lineSegment line) of
+                                    Just i ->
+                                        Debug.log "intersection" ( i, False )
+
+                                    Nothing ->
+                                        ( newPosition, newFalling )
+
+                            ( newPosition, newFalling ) =
+                                List.foldr adjust ( newPlayer.position, newPlayer.falling ) lines
+                        in
+                            { newPlayer
+                                | position = newPosition
+                                , falling = newFalling
+                            }
+                    )
+    in
+        { newState | players = players }
+
+
+{-| Compute new State.
+-}
+step : Time -> State -> State
+step dt state =
+    state
+        |> physics dt
+        |> collision dt state
+        |> camera dt
+
+
+
+-- TODO: currentTime
+
+
+camera : Time -> State -> State
+camera dt state =
+    let
+        currentTime =
+            state.tcamera.lastFrame + dt
+
+        player =
+            state.players
+                |> List.drop state.activePlayer
+                |> List.head
+                |> Maybe.withDefault defaultPlayer
+    in
+        { state
+            | tcamera =
+                Camera.step currentTime state.tcamera
+                    |> Camera.retarget currentTime
+                        (let
+                            pointOfInterest =
+                                vec2 500 150
+
+                            p =
+                                Vec2.length (Vec2.sub player.position pointOfInterest) <= 100
+                         in
+                            if p then
+                                { defaultCamera
+                                    | position = player.position
+                                    , scale = 2.5
+                                }
+                            else
+                                { defaultCamera
+                                    | position = player.position
+                                    , scale = 1
+                                }
+                        )
+        }
+
+
+
+-- DRAW
+
+
+draw : Time -> State -> Html Cmd
+draw dt state =
+    let
+        newState =
+            step dt state
+    in
+        Svg.svg
+            [ Svg.width "600"
+            , Svg.height "400"
+            , Svg.viewBox "0 0 600 400"
+            , Svg.transform (Camera.transform state.tcamera.lastCamera)
+            ]
+            (List.concat
+                [ state.players
+                    |> List.indexedMap (,)
+                    |> List.sortBy
+                        (\( i, r ) ->
+                            if i == state.activePlayer then
+                                1
+                            else
+                                0
+                        )
+                    |> List.map (\( i, r ) -> drawPlayer r)
+                    |> List.concat
+                , [ drawGround state ]
+                ]
+            )
+            |> \svg ->
+                Html.div []
+                    [ svg
+                    , Html.button [ Events.onClick (Load "level0.json") ] [ Html.text "level 0" ]
+                    , Html.button [ Events.onClick (Load "level1.json") ] [ Html.text "level 1" ]
+                    ]
+
+
+drawPlayer : Player -> List (Svg m)
+drawPlayer player =
+    let
+        drawCorner corner attr =
+            Svg.circle
+                (attr
+                    ++ [ Svg.cx <| toString <| getX <| computePosition corner player
+                       , Svg.cy <| toString <| getY <| computePosition corner player
+                       ]
+                )
+                []
+    in
+        [ Svg.rect
+            [ Svg.x <| toString (getX player.position - 10)
+            , Svg.y <| toString (getY player.position - player.height)
+            , Svg.width "20"
+            , Svg.height (player.height |> toString)
+            , Svg.fill player.color
+            ]
+            []
+        , drawCorner LowerLeft
+            [ Svg.fill player.color
+            , Svg.r "3"
+            ]
+        , drawCorner LowerRight
+            [ Svg.fill player.color
+            , Svg.r "3"
+            ]
+        , drawCorner A
+            [ Svg.fill "green"
+            , Svg.r "1"
+            ]
+        , drawCorner B
+            [ Svg.r "1" ]
+        , drawCorner C
+            [ Svg.r "1" ]
+        , drawCorner D
+            [ Svg.r "1" ]
+        ]
+
+
+drawGround : State -> Svg m
+drawGround state =
+    let
+        drawLine line =
+            Svg.line
+                [ Svg.x1 <| toString <| getX line.left
+                , Svg.y1 <| toString <| getY line.left
+                , Svg.x2 <| toString <| getX line.right
+                , Svg.y2 <| toString <| getY line.right
+                , Svg.width "1"
+                , Svg.stroke "brown"
+                ]
+                []
+
+        lines =
+            List.concat
+                [ state.ground
+                , state.players
+                    |> List.map
+                        (\player ->
+                            { left = player.position |> add (vec2 (-10) (-player.height))
+                            , right = player.position |> add (vec2 10 (-player.height))
+                            }
+                        )
+                ]
+    in
+        Svg.g [] <| List.map drawLine lines
+
+
+
+-- CMD
+
+
+type Cmd
+    = NoOp
+    | Jump
+    | Move Direction
+    | Halt
+    | Cycle
+    | Load String
+    | LoadOk (Result Http.Error (List Line))
+
+
+type Direction
+    = Left
+    | Right
+
+
+
+-- EXECUTE
+
+
+execute : Cmd -> State -> ( State, Cmd.Cmd Cmd )
+execute cmd state =
+    case cmd of
+        NoOp ->
+            state ! []
+
+        Jump ->
+            let
+                players =
+                    state.players
+                        |> List.indexedMap
+                            (\i player ->
+                                if i == state.activePlayer then
+                                    { player | velocity = -0.3 }
+                                else
+                                    player
+                            )
+            in
+                { state | players = players } ! []
+
+        Move direction ->
+            { state | movement = Just direction } ! []
+
+        Halt ->
+            { state | movement = Nothing } ! []
+
+        Cycle ->
+            let
+                activePlayer =
+                    state.activePlayer
+                        + 1
+                        |> flip (%) (List.length state.players)
+            in
+                { state
+                    | activePlayer = activePlayer
+                }
+                    ! []
+
+        Load fn ->
+            let
+                parse1 xs =
+                    case xs of
+                        x :: y :: xs_ ->
+                            vec2 (10 * x) (-10 * y + 500) :: parse1 xs_
+
+                        _ ->
+                            []
+
+                parse2 xs =
+                    case xs of
+                        u :: v :: xs_ ->
+                            { left = u, right = v } :: parse2 (v :: xs_)
+
+                        _ ->
+                            []
+
+                parse =
+                    List.map parse1 >> List.map parse2 >> List.concat
+
+                decodeLevel =
+                    Json.list (Json.list (Json.float)) |> Json.map parse
+            in
+                state ! [ Http.get fn decodeLevel |> Http.send LoadOk ]
+
+        LoadOk (Ok ground) ->
+            { state
+                | ground = ground
+            }
+                ! []
+
+        LoadOk (Err _) ->
+            state ! []
+
+
+
+-- KEYBIND
+
+
+bind : Keyboard.KeyCode -> Bool -> Cmd
+bind keycode pressed =
+    if pressed then
+        case keycode of
+            -- spacebar
+            32 ->
+                Jump
+
+            -- left
+            37 ->
+                Move Left
+
+            -- right
+            39 ->
+                Move Right
+
+            -- tab
+            9 ->
+                Cycle
+
+            _ ->
+                NoOp
+    else
+        case keycode of
+            -- left
+            37 ->
+                Halt
+
+            -- right
+            39 ->
+                Halt
+
+            _ ->
+                NoOp
+
+
+
+-- MAIN
+
+
+type alias Jump =
+    Engine.GameLogic State Cmd
+
+
+jump : Jump
+jump =
+    { defaultState = defaultState
+    , execute = execute
+    , bind = bind
+    , step = step
+    , draw = draw
+    , init = init
+    }
+
+
+init =
+    ( defaultState, Cmd.none )
+
+
+main : Program Never (Engine.Model State Cmd) (Engine.Msg Cmd)
+main =
+    Engine.engine jump
--- src/Player.elm
+++ src/Player.elm
@@ -1,21 +1,172 @@
 module Player exposing (..)
 
-import Math.Vector2 exposing (Vec2, vec2)
+import Math.Vector2 exposing (..)
 
+
+{-| State of one player entity.  Position is given wrt the center.
+Rotation tells how much radians it is rotated counterclockwise.
+-}
 type alias Player =
     { position : Vec2
+    , width : Float
+    , height : Float
+    , rotation : Float
     , velocity : Float
     , color : String
     , active : Bool
-    , height : Float
+    , falling : Bool
     }
 
 
 defaultPlayer : Player
 defaultPlayer =
     { position = vec2 0 0
+    , width = 40
+    , height = 20
+    , rotation = 0
     , velocity = 0
     , color = "#000"
     , active = False
-    , height = 20
+    , falling = True
     }
+
+
+{-| Type to select the corner of a Player.  LowerLeft, ..., UpperRight
+are with respect to the current rotation.  A, ..., D are always the same
+corners.  If rotation == 0, we have A = LowerLeft, B = LowerRight,
+C = UpperRight, D == UpperLeft.  (counterclockwise)
+-}
+type Corner
+    = LowerLeft
+    | LowerRight
+    | UpperRight
+    | UpperLeft
+    | A
+    | B
+    | C
+    | D
+
+
+computePosition : Corner -> Player -> Vec2
+computePosition corner player =
+    let
+        x =
+            player.width / 2
+
+        y =
+            player.height / 2
+
+        permutation =
+            (floor (2 * player.rotation / pi + 1 / 2)) % 4
+
+        rotateAndShift dx dy =
+            add
+                (rotate player.rotation (vec2 dx dy))
+                player.position
+
+        ( a, b, c, d ) =
+            ( rotateAndShift -x y
+            , rotateAndShift x y
+            , rotateAndShift x -y
+            , rotateAndShift -x -y
+            )
+
+        ( ll, lr, ur, ul ) =
+            case permutation of
+                0 ->
+                    ( a, b, c, d )
+
+                1 ->
+                    ( d, a, b, c )
+
+                2 ->
+                    ( c, d, a, b )
+
+                3 ->
+                    ( b, c, d, a )
+
+                _ ->
+                    Debug.crash "this permutation is not possible"
+    in
+        case corner of
+            A ->
+                a
+
+            B ->
+                b
+
+            C ->
+                c
+
+            D ->
+                d
+
+            LowerLeft ->
+                ll
+
+            LowerRight ->
+                lr
+
+            UpperRight ->
+                ur
+
+            UpperLeft ->
+                ul
+
+
+{-| Rotate the vector counterclockwise.
+-}
+rotate : Float -> Vec2 -> Vec2
+rotate angle v =
+    let
+        ( x, y ) =
+            toTuple v
+    in
+        vec2
+            (cos angle * x + sin angle * y)
+            (-1 * sin angle * x + cos angle * y)
+
+
+type alias LineSegment =
+    { a : Vec2
+    , b : Vec2
+    }
+
+
+intersection : LineSegment -> LineSegment -> Maybe Vec2
+intersection l1 l2 =
+    let
+        v =
+            sub l1.b l1.a
+
+        w =
+            sub l2.b l2.a
+
+        d =
+            sub l2.a l1.a
+
+        n =
+            crossProduct v w
+    in
+        if n == 0 then
+            Nothing
+        else
+            let
+                s =
+                    crossProduct d v / crossProduct v w
+
+                t =
+                    crossProduct d w / crossProduct v w
+
+                between a b z =
+                    (z > a) && (z < b)
+            in
+                if (s |> between 0 1) && (t |> between 0 1) then
+                    Just (add l1.a (scale s v))
+                else
+                    Nothing
+
+
+crossProduct : Vec2 -> Vec2 -> Float
+crossProduct v w =
+    (getX v) * (getY w) - (getX w) * (getY v)
